cc03c0303223918d3c9013feb3655b96164e359a
Franz-SuperCode <franzsupercode@gmail.com>
2022-11-01 11:44:30 +0100

    small changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,11 @@ function changeColor(event) {
     let body = document.querySelector("body");
     body.classList.toggle("allBlack");
 
+    document.querySelector(".containerHowItWorks").style.color = "black";
+    document.querySelector(".a1").style.color = "white";
+    document.querySelector(".a2").style.color = "white";
+    document.querySelector(".a3").style.color = "white";
+    document.querySelector(".a4").style.color = "white";
+
+
 }
